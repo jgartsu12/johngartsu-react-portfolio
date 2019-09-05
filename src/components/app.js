9e50699fa95b83a56a7049ca676a1ce454fd04ba
jgartsu12 <jgartsu12@gmail.com>
2019-09-05 12:37:04 -0400

    creating props

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import moment from "moment";
 
+import PortfolioContainer from "./portfolio/portfolio-container";
+
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
         <h1>John Gartsu Portfolio</h1>
-        <div>
-          {moment().format('MMMM Do YYYY, h:mm:ss a')}
-        </div>
+        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div> 
+        <PortfolioContainer />
       </div>
     );
   }
