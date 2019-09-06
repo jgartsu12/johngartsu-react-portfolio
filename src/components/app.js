c442eac2eeb55b4c1d81064309753c7db8b55344
jgartsu12 <jgartsu12@gmail.com>
2019-09-06 12:29:49 -0400

    ternary operators/ top nav bar made

--- src/components/app.js
+++ src/components/app.js
@@ -1,14 +1,17 @@
-import React, { Component } from 'react';
+
+import React, { Component } from "react";
 import moment from "moment";
 
 import PortfolioContainer from "./portfolio/portfolio-container";
+import NavigationContainer from "./navigation/navigation-container";
 
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>John Gartsu Portfolio</h1>
-        <div>{moment().format('MMMM Do YYYY, h:mm:ss a')}</div> 
+      <div className="app">
+        <NavigationContainer />
+        <h1>Jordan Hudgens Portfolio</h1>
+        <div>{moment().format("MMMM Do YYYY, h:mm:ss a")}</div>
         <PortfolioContainer />
       </div>
     );
---
+++ src/components/navigation/navigation-container.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from "react";
+
+export default class NavigationComponent extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <button>Home</button>
+        <button>About</button>
+        <button>Contact</button>
+        <button>Blog</button>
+        {false ? <button>Add Blog</button> : null}
+      </div>
+    );
+  }
+}
