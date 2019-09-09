5638ab05db85d72fb0351af06862e3abc5ba02b1
jgartsu12 <jgartsu12@gmail.com>
2019-09-09 12:36:47 -0400

    fixed bug; babel install needed for axios install to run server

--- src/components/portfolio/portfolio-container.js
+++ src/components/portfolio/portfolio-container.js
@@ -36,7 +36,7 @@ export default class PortfolioContainer extends Component {
 
   getPortfolioItems() {
     axios
-      .get("https://jordan.devcamp.space/portfolio/portfolio_items")
+      .get("https://johngartsu.devcamp.space/portfolio/portfolio_items")
       .then(response => {
         console.log("response data", response);
       })
