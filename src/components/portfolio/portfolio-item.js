646d6938c5e30b508c1e01c14bb646945f500b85
jgartsu12 <jgartsu12@gmail.com>
2019-09-12 12:03:38 -0400

    built style structure for home

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -2,15 +2,9 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 export default function(props) {
-  // Data that we'll need:
-  // - background image: thumb_image_url
-  // - logo
-  // - description: description
-  // - id: id
-
   const { id, description, thumb_image_url, logo } = props.item;
   return (
-    <div>
+    <div className="portfolio-item-wrapper">
       <img src={thumb_image_url} />
       <img src={logo} />
       <div>{description}</div>
