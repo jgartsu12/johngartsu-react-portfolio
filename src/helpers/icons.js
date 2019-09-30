37001b2f5a841838f253004d6688e7f119fdb106
jgartsu12 <jgartsu12@gmail.com>
2019-09-30 09:56:23 -0400

    built contact page styles

--- src/helpers/icons.js
+++ src/helpers/icons.js
@@ -1,14 +1,26 @@
 import {
+  faTrash,
+  faSignOutAlt,
+  faEdit,
+  faSpinner,
+  faPlusCircle,
+  faPhone,
+  faEnvelope,
+  faMapMarkedAlt
+} from "@fortawesome/free-solid-svg-icons";
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+const Icons = () => {
+  return library.add(
     faTrash,
     faSignOutAlt,
     faEdit,
     faSpinner,
-    faPlusCircle
-  } from "@fortawesome/free-solid-svg-icons";
-  import { library } from "@fortawesome/fontawesome-svg-core";
-  
-  const Icons = () => {
-    return library.add(faTrash, faSignOutAlt, faEdit, faSpinner, faPlusCircle);
-  };
-  
-  export default Icons;
+    faPlusCircle,
+    faPhone,
+    faEnvelope,
+    faMapMarkedAlt
+  );
+};
+
+export default Icons;
