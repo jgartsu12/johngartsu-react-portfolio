5e299c412ef1e7c1ad740fe5e3719eb8db69c8fe
jgartsu12 <jgartsu12@gmail.com>
2020-07-31 09:35:10 -0400

    added about page content

--- src/components/pages/about.js
+++ src/components/pages/about.js
@@ -13,24 +13,26 @@ export default function() {
         }}
       />
       <div className="right-column">
-        Maecenas faucibus mollis interdum. Integer posuere erat a ante venenatis
-        dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis.
-        Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui.
-        Aenean lacinia bibendum nulla sed consectetur. Maecenas sed diam eget
-        risus varius blandit sit amet non magna. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros. Donec id elit non mi porta gravida
-        at eget metus. Donec sed odio dui. Cras mattis consectetur purus sit
-        amet fermentum. Etiam porta sem malesuada magna mollis euismod. Nulla
-        vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque
-        ornare sem lacinia quam venenatis vestibulum. Duis mollis, est non
-        commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec
-        elit. Praesent commodo cursus magna, vel scelerisque nisl consectetur
-        et. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent
-        commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis
-        risus eget urna mollis ornare vel eu leo. Morbi leo risus, porta ac
-        consectetur ac, vestibulum at eros. Vestibulum id ligula porta felis
-        euismod semper.
+        John Gartsu is recent graduate from Bottega Tech (July 2020) with a previous B.S. degree in Exercise Science 
+        with a concentration in Human Performance from Southern Connecticut State University (Dec 2015). 
+        Also, he served in the Air Force National Guard in Connecticut as Structural Engineer while he was earning his 
+        Bachelors degree. When John left the Air Force and graduated from college, he found himself doing various careers.
+        While he was on his journey to finding his passion, he was teaching himself coding through YouTube, Codecademy, and 
+        FreeCodeCamp. 
+        
+        He decided to get a certificate in Full Stack in Web Engineering from Bottega Tech.
+        Once he joined Bottega Tech and met various mentors from the program, he found his passion in Software Engineering.
+        During his time at Bottega Tech, he learned various languages and frameworks such as Python3, JavaScript, ReactJS, and
+        Flask. His favorite language is Python. He enjoys teaching himself new coding languages and frameworks, and has a strong desire to learn.
+        John is teaching himself the programming languages C#, GOlang, and .NET framework through Codecademy and by reading
+        documentation. John believes he learns best by reading the documentation and creating projects. 
+
+        John also enjoys teaching himself Information Technology concepts. He is taking classes online to receive certifications 
+        in COMPTIA A+, Networking+, Security+. Clearly, he has a strong passion to learn.
+
+        Currently, John works at Awareness Technologies Incorporated as a Tier 2 Technical Analyst.
+        At Awareness Technologies Inc, he provides Tier 2 support for InterGuard employee monitoring software.
       </div>
     </div>
-  );
+  ); 
 }
